fed16063ae0bdf46c5ff06b09cc670d29d7e7fac
R <R>
2026-06-27 11:22:34 +0100

    Initial Commit

---
+++ FinalFarewellTests/FinalFarewellTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinalFarewellTests.swift
+//  FinalFarewellTests
+//
+//  Created by Ross Ferguson on 27/06/2026.
+//
+
+import Testing
+@testable import FinalFarewell
+
+struct FinalFarewellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
